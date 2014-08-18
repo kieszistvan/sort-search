334e73978a5ac5ad8804fa236c3f8c2150606602
Istvan Kiesz <kieszistvan@gmail.com>
2014-08-18 22:16:29 +0200

    insertion sort + test

--- src/main/scala/sorting/Sort.scala
+++ src/main/scala/sorting/Sort.scala
@@ -1,37 +1,55 @@
 package sorting
 
 object Sort {
-	
-  def emptyChecked(xs: List[Int])(f: List[Int] => List[Int]): List[Int] = if(xs.isEmpty) List() else f(xs)
-  
-  def bubble(xs: List[Int]): List[Int] = emptyChecked(xs) { xs => 
-	
-    def inner(ys: List[Int], i: Int, switched: Boolean): List[Int] = {
-		if (ys.size-1 == i) {
-			if (switched) inner(ys, 0, false) else ys
-		}	else if (ys(i) > ys(i+1)) {
-			val zs = ys.take(i) ::: List(ys(i+1)) ::: List(ys(i)) ::: ys.drop(i+2)
-			inner(zs, i+1, true) 
-		} else inner(ys, i+1, switched)
-	}
-	
-	inner(xs, 0, false)
-  }                                         
 
-  def isSorted(xs: List[Int]):Boolean = {
+  def emptyChecked(xs: List[Int])(f: List[Int] => List[Int]): List[Int] = if (xs.isEmpty) List() else f(xs)
+
+  def bubble(xs: List[Int]): List[Int] = emptyChecked(xs) { xs =>
+
+    def inner(ys: List[Int], i: Int, switched: Boolean): List[Int] = {
+      if (ys.size - 1 == i) {
+        if (switched) inner(ys, 0, false) else ys
+      } else if (ys(i) > ys(i + 1)) {
+        val zs = ys.take(i) ::: List(ys(i + 1)) ::: List(ys(i)) ::: ys.drop(i + 2)
+        inner(zs, i + 1, true)
+      } else inner(ys, i + 1, switched)
+    }
+
+    inner(xs, 0, false)
+  }
+
+  def isSorted(xs: List[Int]): Boolean = {
     if (xs.isEmpty) return true
-    
+
     try {
       xs.fold(xs.head)((a, b) => if (a <= b) b else throw new SortingException)
-      return true
+      true
     } catch {
       case e: SortingException => return false
-      case _:Throwable => throw new RuntimeException
+      case _: Throwable => throw new RuntimeException
     }
   }
- 
-  
-  
+
+  def insertion(xs: List[Int]): List[Int] = emptyChecked(xs) {
+    xs =>
+
+      def inner(sorted: List[Int], unsorted: List[Int]): List[Int] = {
+        if (unsorted.isEmpty) return sorted
+        inner(put(unsorted.head, sorted), unsorted.tail)
+      }
+
+      // put the element into a sorted list
+      def put(elem: Int, ys: List[Int]): List[Int] = {
+        ys match {
+          case h :: t =>
+            if (elem > h) h :: put(elem, t)
+            else elem :: ys
+          case Nil => ys ::: List(elem)
+        }
+      }
+      inner(Nil, xs)
+  }
+
 }
 
 class SortingException extends RuntimeException
--- src/test/scala/sorting/SortSpec.scala
+++ src/test/scala/sorting/SortSpec.scala
@@ -3,45 +3,60 @@ package sorting
 import org.specs2.mutable._
 
 class SortSuite extends Specification {
-	import Sort._
+  import Sort._
 
-	"Sort#bubble" should {
-	  "sort empty list without failure" in {
-	    val empty = List[Int]()
-	    bubble(empty) must equalTo(List())
-	  } 
-	  "sort unsorted list" in {
-	    val unsorted = List(4,9,1,3,8)
-	    bubble(unsorted) must equalTo(List(1,3,4,8,9))
-	  }
-	  "sort list with negative integers" in {
-	    val unsortedWithNegative = List(9,8,-3,2,-9)
-	    bubble(unsortedWithNegative) must equalTo(List(-9, -3, 2, 8 ,9))
-	  }
-	  "sort list with repeating integers" in {
-	    val unsortedWithRepeatingInts = List(0,1,0,1,0,1,2,2,-1)
-	    bubble(unsortedWithRepeatingInts) must equalTo(List(-1,0,0,0,1,1,1,2,2))
-	  }
-	}
-	
-	"Sort#isSorted" should {
-	  "consider empty list sorted" in {
-	    val empty = List()
-	    isSorted(empty) must equalTo(true)
-	  }
-	  "recognize correctly sorted list" in {
-	    val sorted = List(1,2,3,9,12)
-	    isSorted(sorted) must equalTo(true)
-	  }
-	  "recognize if list is not sorted" in {
-	    val unsorted = List(1,3,2,0)
-	    isSorted(unsorted) must equalTo(false)
-	  }
-	  "recognize sorted list with repeating ints" in {
-	    val sortedWithRepeating = List(1,1,3,9,9)
-	    isSorted(sortedWithRepeating) must equalTo(true)
-	  }
-	}
-	
+  "Sort#bubble" should {
+    "sort empty list without failure" in {
+      val empty = List[Int]()
+      bubble(empty) must equalTo(List())
+    }
+    "sort unsorted list" in {
+      val unsorted = List(4, 9, 1, 3, 8)
+      bubble(unsorted) must equalTo(List(1, 3, 4, 8, 9))
+    }
+    "sort list with negative integers" in {
+      val unsortedWithNegative = List(9, 8, -3, 2, -9)
+      bubble(unsortedWithNegative) must equalTo(List(-9, -3, 2, 8, 9))
+    }
+    "sort list with repeating integers" in {
+      val unsortedWithRepeatingInts = List(0, 1, 0, 1, 0, 1, 2, 2, -1)
+      bubble(unsortedWithRepeatingInts) must equalTo(List(-1, 0, 0, 0, 1, 1, 1, 2, 2))
+    }
+  }
+
+  "Sort#isSorted" should {
+    "consider empty list sorted" in {
+      val empty = List()
+      isSorted(empty) must equalTo(true)
+    }
+    "recognize correctly sorted list" in {
+      val sorted = List(1, 2, 3, 9, 12)
+      isSorted(sorted) must equalTo(true)
+    }
+    "recognize if list is not sorted" in {
+      val unsorted = List(1, 3, 2, 0)
+      isSorted(unsorted) must equalTo(false)
+    }
+    "recognize sorted list with repeating ints" in {
+      val sortedWithRepeating = List(1, 1, 3, 9, 9)
+      isSorted(sortedWithRepeating) must equalTo(true)
+    }
+  }
+
+  "Sort#insertion" should {
+    "sort empty list without failure" in {
+      val empty = List()
+      insertion(empty) must equalTo(List())
+    }
+    "sort unsorted list" in {
+      val unsorted = List(4, 1, 9, 0)
+      insertion(unsorted) must equalTo(List(0, 1, 4, 9))
+    }
+    "sort list with repeating integers" in {
+      val unsortedWithRepeatingInts = List(0, 1, 0, 1, 0, 1, 2, 2, -1)
+      insertion(unsortedWithRepeatingInts) must equalTo(List(-1, 0, 0, 0, 1, 1, 1, 2, 2))
+    }
+
+  }
 
 }
