75dc5cd059353029aba7d717923625c2499e97b4
Istvan Kiesz <kieszistvan@gmail.com>
2014-08-18 13:20:28 +0200

    moved

--- src/main/scala/sorting/Sort.scala
+++ src/main/scala/sorting/Sort.scala
@@ -10,7 +10,7 @@ object Sort {
 			if (switched) inner(ys, 0, false) else ys
 		}	else if (ys(i) > ys(i+1)) {
 			val zs = ys.take(i) ::: List(ys(i+1)) ::: List(ys(i)) ::: ys.drop(i+2)
-			inner(zs, i+1, true)
+			inner(zs, i+1, true) 
 		} else inner(ys, i+1, switched)
 	}
 	
